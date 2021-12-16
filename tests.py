a19776d17c2edf4718b8f77a8df5a69d3e2ee9c1
easy-quest <esim.i2p@gmail.com>
2021-12-16 21:39:22 +0000

    pod

--- tests.py
+++ tests.py
@@ -7,9 +7,48 @@
 
 import random
 from time import sleep
+from pprint import pprint
+from itertools import cycle
 
 from requests_html import HTMLSession  
-session = HTMLSession()  
-r = session.get('https://www.novostibankrotstva.ru')
 
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    "accept": "*/*"
+}
+
+
+list_proxy = [
+        'socks5://127.0.0.1:9050',
+		'socks5://127.0.0.1:9052',
+		'socks5://127.0.0.1:9053',
+		'socks5://127.0.0.1:9054',
+    ]
+
+proxy_cycle = cycle(list_proxy)
+proxy = next(proxy_cycle)
+
+for i in range(1, 100000):
+	proxy = next(proxy_cycle)
+	
+proxies = {
+		"http": proxy,
+		"https": proxy
+    }
+    
+session = HTMLSession()
+    
+# r = session.get('https://www.novostibankrotstva.ru', headers=headers, proxies=proxies)
+r = session.get('https://check.torproject.org/', headers=headers, proxies=proxies)
+sleep(1)
+
+# pprint(r.html.links)
+# pprint(r.html.html)
+# print(r.html.find('title'))
+# title = r.html.xpath('title')
+# print(title.text)
+# print(r.html.text)
+r.html.render()  
+print(r.html.html) 
 # r.html.render(sleep=2, scrolldown=1)
+
